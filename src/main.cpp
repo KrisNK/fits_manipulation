97411b1df75c946d91ee48dff58fcfbde9025377
Kristofer Karam <kristofer.karam@gmail.com>
2020-05-21 17:05:30 -0400

    Modified Makefile to work

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@
 int main()
 {
     fits_manipulation fitsfiles;
-    std::string target = "~/Desktop/output.fits";
-    std::string path1 = "../test.fit";
-    std::string path2 = "test2.fit";
-    std::string path3 = "test3.fit";
-    std::string path4 = "test4.fit";
-    std::string path5 = "test5.fit";
+    std::string target = "/home/kris/Desktop/output.fits";
+    std::string path1 = "../res/test.fit";
+    std::string path2 = "../res/test2.fit";
+    std::string path3 = "../res/test3.fit";
+    std::string path4 = "../res/test4.fit";
+    std::string path5 = "../res/test5.fit";
     fitsfiles.extractFitsData(&path1);
     fitsfiles.extractFitsData(&path2);
     fitsfiles.extractFitsData(&path3);
